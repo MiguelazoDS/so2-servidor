6b65ebd39d418605177f80c0ea45686bc85ead0e
Cazajous Miguel A <migue7490@gmail.com>
2018-06-02 13:11:10 -0300

    bash.pl executes bash binary with arguments, reads the file output and prints it

--- Códigos/bash.pl
+++ Códigos/bash.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
 
+=pod
 my $data;
 use File::Slurp;
 BEGIN {
@@ -29,3 +30,31 @@ print "<a href=$buffer>DOWNLOAD FILE</a>";
 print "</body>";
 print "</html>";
 1;
+=cut
+
+use CGI;
+use Cwd;
+$q = CGI->new;
+print 	$q->header( -charset=>'utf-8'),
+     	  $q->start_html( -title=>'pc_info',
+                        -style=>{-src=>'style.css'}),
+     	  $q->end_html;
+
+$buffer = $ENV{'QUERY_STRING'};
+if($buffer =~ /\w=(.+)/){
+  $buffer=$1;
+}
+$buffer="df -h";
+system("Bash/Bash $buffer");
+
+sub get_info{
+	@h= split "\n", `cat $_[0]`;
+	foreach $a(@h){
+  	print $q->p($a),
+       	$q->end_html;
+	}
+}
+
+$out="output";
+print $q->h3("SALIDA BASH");
+get_info($out);
